00294685a0b2aebd8f9e1f2390826fcf15f265b3
Harry Bosch <harrybosch03@gmail.com>
2023-06-07 16:22:36 +1000

    no message

---
+++ Assets/Code/Scripts/Interactables/PlayerChair.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceTruckBongSimulator
+{
+    [SelectionBase]
+    [DisallowMultipleComponent]
+    public sealed class PlayerChair : MonoBehaviour
+    {
+        
+    }
+}
